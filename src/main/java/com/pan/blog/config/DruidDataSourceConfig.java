bd263ae910b30101ebc92bd0a36ff8e1e7050e0c
FantasticPan <706390664@qq.com>
2018-12-15 21:07:27 +0800

    集成druid连接池

---
+++ src/main/java/com/pan/blog/config/DruidDataSourceConfig.java
@@ -0,0 +1,116 @@
+package com.pan.blog.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by FantasticPan on 2018/12/15.
+ */
+@Configuration
+@Slf4j
+public class DruidDataSourceConfig {
+
+    // 获取yml配置文件中的配置信息
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.druid.filters}")
+    private String filters;
+    @Value("${spring.datasource.druid.connection-properties}")
+    private String connectProperties;
+    @Value("${spring.datasource.druid.initial-size}")
+    private Integer initialSize;
+    @Value("${spring.datasource.druid.min-idle}")
+    private Integer minIdle;
+    @Value("${spring.datasource.druid.max-active}")
+    private Integer maxActive;
+    @Value("${spring.datasource.druid.max-wait}")
+    private Integer maxWait;
+    @Value("${spring.datasource.druid.time-between-eviction-runs-millis}")
+    private Integer timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.druid.min-evictable-idle-time-millis}")
+    private Integer minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.druid.validation-query}")
+    private String validationQuery;
+    @Value("${spring.datasource.druid.test-while-idle}")
+    private Boolean testWhileIdle;
+    @Value("${spring.datasource.druid.test-on-borrow}")
+    private Boolean testOnBorrow;
+    @Value("${spring.datasource.druid.test-on-return}")
+    private Boolean testOnReturn;
+    @Value("${spring.datasource.druid.pool-prepared-statements}")
+    private Boolean poolPreparedStatements;
+
+    /**
+     * 由于SpringBoot和Druid兼容性问题，手动配置连接池
+     */
+    @Bean
+    @Primary // 不能保证只有一个实例，@Primary注解的实例优先于其他实例被注入
+    public DataSource dataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driver);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        datasource.setPoolPreparedStatements(poolPreparedStatements);
+        datasource.setConnectionProperties(connectProperties);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error("druid filters error:" + e);
+        }
+        return datasource;
+    }
+
+    /**
+     * 监控页面 ip + 端口/druid/index.htm
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        //创建servlet注册实体
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername", "lipan");
+        servletRegistrationBean.addInitParameter("loginPassword", "lp19970622");
+        //是否能够重置数据 禁用HTML页面上的“Reset All”功能
+        servletRegistrationBean.addInitParameter("resetEnable", "true");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
