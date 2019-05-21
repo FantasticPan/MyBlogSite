12b21ebe9f8314459c11faae49aec3f3e1d58f54
FantasticPan <706390664@qq.com>
2019-05-21 13:14:12 +0800

    解决异常信息：java.lang.IllegalArgumentException: Invalid character found in the request target. The valid characters are defined in RFC 7230 and RFC 3986

---
+++ src/main/java/com/pan/blog/config/Config.java
@@ -0,0 +1,34 @@
+package com.pan.blog.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by FantasticPan on 2019/5/21.
+ */
+@Configuration
+public class Config {
+
+    /**
+     * 解决异常信息：
+     *  java.lang.IllegalArgumentException:
+     *      Invalid character found in the request target. The valid characters are defined in RFC 7230 and RFC 3986
+     * @return
+     */
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        });
+
+        return factory;
+    }
+}
